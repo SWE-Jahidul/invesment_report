26d4a787b5641c37a7e6415b8e94b31ac8de92ee
jahidul <programmerjahid162@gmail.com>
2022-07-01 20:53:59 +0600

    adding search page table

--- src/Component/Search.js
+++ src/Component/Search.js
@@ -27,7 +27,7 @@ const Search = () => {
             <div class="input-group ">
               <input
                 type="text"
-                class="form-control "
+                className="form-control "
                 placeholder="Search..."
                 aria-label="Recipient's username"
                 aria-describedby="basic-addon2"
@@ -36,7 +36,7 @@ const Search = () => {
                   border: "1px solid #00ADEE",
                 }}
               />
-              <div class="input-group-append ">
+              <div className="input-group-append ">
                 <button
                   style={{
                     backgroundColor: "#00ADEE",
@@ -52,21 +52,17 @@ const Search = () => {
             </div>
             <div>
               <div class="accordion-item">
-                
-                  <button
-                    className="accordion-button collapsed"
-                    type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#advanceSearch"
-                    
-                    style={{
-                      fontSize: 20,
-                      height: 53,
-                      backgroundColor: '#00adee'
-                    }}
-                  >
-                    
-                  </button>
+                <button
+                  className="accordion-button collapsed"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#advanceSearch"
+                  style={{
+                    fontSize: 20,
+                    height: 53,
+                    backgroundColor: "#00adee",
+                  }}
+                ></button>
                 {/* </h2> */}
               </div>
             </div>
@@ -285,6 +281,194 @@ const Search = () => {
         </div>
       </section>
       {/* Advance Search Section End  */}
+
+      {/* Start table of All content  */}
+
+      <section
+        className="container mt-5"
+        style={{
+          backgroundColor: "#222222",
+          padding: 25,
+        }}
+      >
+        <ul
+          className="nav nav-pills mb-3 d-flex justify-content-center"
+          id="pills-tab"
+          role="tablist"
+        >
+          <li className="nav-item" role="presentation">
+            <button
+              className="nav-link active"
+              id="pills-home-tab"
+              data-bs-toggle="pill"
+              data-bs-target="#pills-home"
+              type="button"
+              role="tab"
+              aria-controls="pills-home"
+              aria-selected="true"
+            >
+              Issuser Name
+            </button>
+          </li>
+          <li className="nav-item" role="presentation">
+            <button
+              className="nav-link"
+              id="pills-profile-tab"
+              data-bs-toggle="pill"
+              data-bs-target="#pills-profile"
+              type="button"
+              role="tab"
+              aria-controls="pills-profile"
+              aria-selected="false"
+            >
+              Deal Name
+            </button>
+          </li>
+          <li className="nav-item" role="presentation">
+            <button
+              className="nav-link"
+              id="pills-contact-tab"
+              data-bs-toggle="pill"
+              data-bs-target="#pills-contact"
+              type="button"
+              role="tab"
+              aria-controls="pills-contact"
+              aria-selected="false"
+            >
+              Financer Name
+            </button>
+          </li>
+        </ul>
+        <div className="tab-content" id="pills-tabContent">
+          <div
+            className="tab-pane fade show active text-white"
+            id="pills-home"
+            role="tabpanel"
+            aria-labelledby="pills-home-tab"
+          >
+            <h3> Issuser Name </h3>
+            <span>4419 Search Results</span>
+
+            <table className="table text-white mt-3">
+              <thead>
+                <tr>
+                  <th scope="col">Issuer Name</th>
+                  <th scope="col">Product Title</th>
+                  <th scope="col">Count</th>
+                  <th></th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th scope="row"> Name of Issuer </th>
+                  <td>Other </td>
+                  <td>1</td>
+                  <td>
+                    {" "}
+                    <i className="bi bi-arrow-up-right-square"> </i>{" "}
+                  </td>
+                </tr>
+                <tr>
+                  <th scope="row"> Name of Issuer </th>
+                  <td>Other </td>
+                  <td>1</td>
+                  <td>
+                    {" "}
+                    <i className="bi bi-arrow-up-right-square"> </i>{" "}
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <div
+            className="tab-pane fade text-white"
+            id="pills-profile"
+            role="tabpanel"
+            aria-labelledby="pills-profile-tab"
+          >
+            <h3> Deal Name </h3>
+            <span>4419 Search Results</span>
+            <table className="table text-white mt-3">
+              <thead>
+                <tr>
+                  <th scope="col">Deal Name</th>
+                  <th scope="col">Issuer Name</th>
+                  <th scope="col">Product</th>
+                  <th scope="col">Contact</th>
+                  <th></th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th scope="row"> Name of Deal </th>
+                  <td>Name of Issuer </td>
+                  <td>Other </td>
+                  <td> Name Name </td>
+                  <td>
+                    {" "}
+                    <i className="bi bi-arrow-up-right-square"> </i>{" "}
+                  </td>
+                </tr>
+                <tr>
+                  <th scope="row"> Name of Deal </th>
+                  <td>Name of Issuer </td>
+                  <td>Other </td>
+                  <td> Name Name </td>
+                  <td>
+                    {" "}
+                    <i className="bi bi-arrow-up-right-square"> </i>{" "}
+                  </td>
+                </tr>
+              </tbody>
+            </table>{" "}
+          </div>
+          <div
+            className="tab-pane fade text-white"
+            id="pills-contact"
+            role="tabpanel"
+            aria-labelledby="pills-contact-tab"
+          >
+            <h3> Financer Name </h3>
+            <span>4419 Search Results</span>
+
+            <table className="table text-white mt-3">
+              <thead>
+                <tr>
+                  <th scope="col">Financer Name</th>
+                  <th scope="col">Issuer Name</th>
+                  <th scope="col">Product</th>
+                  <th scope="col">Contact</th>
+                  <th></th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th scope="row"> Name of Financer </th>
+                  <td>Name of Issuer </td>
+                  <td>Other </td>
+                  <td> Name Name </td>
+                  <td>
+                    {" "}
+                    <i className="bi bi-arrow-up-right-square"> </i>{" "}
+                  </td>
+                </tr>
+                <tr>
+                  <th scope="row"> Name of Financer </th>
+                  <td>Name of Issuer </td>
+                  <td>Other </td>
+                  <td> Name Name </td>
+                  <td>
+                    {" "}
+                    <i className="bi bi-arrow-up-right-square"> </i>{" "}
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </section>
+
+      {/* End table of All content  */}
     </div>
   );
 };
